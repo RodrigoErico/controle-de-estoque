30bc5ab7e09485b394415227c54f7f87ea905681
Rodrigo Érico <rodrigoerico25@gmail.com>
2022-04-29 17:33:15 -0400

    Aplicado todos os migrations

--- application/app_registration/apps.py
+++ application/app_registration/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AppRegistrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_registration'
+    name = 'application.app_registration'
